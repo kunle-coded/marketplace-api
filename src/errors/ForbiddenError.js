ab30d0a81e4572a99978ac6aa717a8049fb5cbb8
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2026-06-03 23:13:19 +0200

    feat(error): implement forbidden error class

---
+++ src/errors/ForbiddenError.js
@@ -0,0 +1,11 @@
+// src/errors/ForbiddenError.js
+
+const AppError = require("./AppError");
+
+class ForbiddenError extends AppError {
+  constructor(message = "Forbidden") {
+    super(message, 403);
+  }
+}
+
+module.exports = ForbiddenError;
--- src/errors/index.js
+++ src/errors/index.js
@@ -4,10 +4,12 @@ const AppError = require("./AppError");
 const BadRequestError = require("./BadRequestError");
 const NotFoundError = require("./NotFoundError");
 const UnauthorizedError = require("./UnauthorizedError");
+const ForbiddenError = require("./ForbiddenError");
 
 module.exports = {
   AppError,
   BadRequestError,
   UnauthorizedError,
   NotFoundError,
+  ForbiddenError,
 };
